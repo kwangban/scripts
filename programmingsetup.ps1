cf4ceb4633f21ca3dce124729ff149b093f1a14e
kwangban <144615636+kwangban@users.noreply.github.com>
2023-09-10 11:42:58 -0500

    Create programmingsetup.ps1

---
+++ programmingsetup.ps1
@@ -0,0 +1,4 @@
+winget install git.git;
+winget install -e --id Microsoft.VisualStudioCode;
+winget install -e --id Microsoft.VisualStudio.2022.Community.Preview;
+winget install -e --id GitHub.GitHubDesktop;
